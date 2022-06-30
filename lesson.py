ef7edf8d82d3f1ae29fee1f95c7f1211668013bb
github <kitakazetoshirokuma@gmail.com>
2022-07-01 00:42:59 +0900

    変数

--- lesson.py
+++ lesson.py
@@ -1,10 +1,62 @@
-num = 1
-name = 'Mike'
-name = '1'
+import math
 
-num = name
-new_num = int(name)
-print(num, type(num))
+result = math.sqrt(25)
+print(result)
 
-print(new_num, type(new_num))
+y = math.log2(10)
+print(y)
+#print(help(math))
 
+print('Hi', 'Mike', sep=',', end='\n')
+
+print('Hi', 'Mike', sep=',', end='.\n')
+
+print('hello')
+print('I don\'t know')
+print('say "I don\'t know"')
+
+print('hello. \nHow are you?')
+
+print('C:\name\name')
+print(r'C:\nam\name')
+
+print("##########")
+print("""\
+line1
+line2
+line3\
+""")
+print("##########")
+
+print("Hi" * 3)
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print('###########')
+print(word[0:2])
+print(word[:2])
+print('###########')
+print(word[2:])
+print(word[2:5])
+
+print('###########')
+word = 'j' + word[1:]
+print(word[:])
+
+n = len(word)
+print(n)
+
+s = 'My name is Bob'
+print(s)
+is_start = s.startswith('My')
+print(is_start)
+print('###########')
+print(s.find('Bob'))
+print(s.rfind('Bob'))
+print(s.count('Bob'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.replace('Mike', 'Nancy'))
