667934466757501210b436092fa33c4aa9d29260
github <kitakazetoshirokuma@gmail.com>
2022-07-13 23:44:19 +0900

    if文

---
+++ section5.py
@@ -0,0 +1,15 @@
+s = 'aaaaaa' \
+    + 'bbbbb'
+print(s)
+
+t = 1 + 2 + 3 + 4 + 5 \
+    + 6 + 7 + 8 + 9 + 10
+print(t)
+
+x = 0
+if x < 0:
+    print('negative')
+elif x ==0:
+    print('0です')
+else:
+    print('positive')
